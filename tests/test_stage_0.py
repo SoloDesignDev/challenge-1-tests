09361ed4b80521540fb74a33fdc58908ff0cb6ae
Solo Design <solodesigndevelopment@gmail.com>
2025-03-30 10:59:34 +0200

    Update test_stage_0.py

--- tests/test_stage_0.py
+++ tests/test_stage_0.py
@@ -1,3 +1,3 @@
 def test_repo_structure():
     import os
-    assert os.path.exists("student/calculator.py"), "Missing calculator.py"
+    assert os.path.exists("../student/calculator.py"), "Missing calculator.py"
